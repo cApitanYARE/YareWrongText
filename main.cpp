4f49424f5a4fd5f530534a2c3a4a3f9a402c7033
cApitanYARE <deniswatch1@gmail.com>
2025-04-17 13:21:36 +0300

    add .ico

--- main.cpp
+++ main.cpp
@@ -8,13 +8,25 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QString imagePath = QDir::currentPath() + "/icon.ico";
+    QString imagePath = QCoreApplication::applicationDirPath();
+    QDir dir(imagePath);
+    dir.cdUp();
+    dir.cdUp();
+    dir.cdUp();
+    QString iconPath = dir.absoluteFilePath("icon.ico");
 
+    qDebug() << iconPath;
     w.setFixedSize(504,307);
     w.setWindowFlags(Qt::FramelessWindowHint);
     w.setAttribute(Qt::WA_TranslucentBackground);
     w.setWindowTitle("Wrong Text");
-    w.setWindowIcon(QIcon(imagePath)); // Шлях до іконки без ресурсу
+    w.setWindowIcon(QIcon(iconPath)); // Шлях до іконки без ресурсу
+    if (QFile::exists(iconPath)) {
+        qDebug() << "Icon exists at:" << iconPath;
+        w.setWindowIcon(QIcon(iconPath));
+    } else {
+        qDebug() << "Icon NOT FOUND at:" << iconPath;
+    }
     w.show();
     return a.exec();
 }
